77b38065b31207f5a719ea6ef7619dcc2c384f02
Anuj Yadav <ay517328@gmail.com>
2023-06-02 14:40:42 +0530

    project started

---
+++ notes.md
@@ -0,0 +1,2 @@
+Jest:is a testing frame work for our code in react.
+web vitals:is a performance measuring tool for measuring performance.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {BiArchive} from 'react-icons/bi'
+
+export default function App() {
+  return (
+    <div className="App">
+<h1> <BiArchive/>Your Appointments</h1>
+    </div>
+  );
+}
